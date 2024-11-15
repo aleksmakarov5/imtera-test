19ee126f1e35952701a017026e5c2a2f1877dbe1
aleksmakarov5 <aleksmakarov5@yandex.ru>
2024-11-16 00:28:15 +0300

    modified:   .DS_Store 	new file:   public/.DS_Store 	modified:   public/favicon.ico 	new file:   public/favicon.png 	modified:   resources/views/welcome.blade.php 	modified:   storage/framework/.DS_Store

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -930,7 +930,7 @@
                         <nav class="-mx-3 flex flex-1 justify-end">
                             @auth
                                 <a href="{{ url('/dashboard') }}"
-                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    style="background-color: #FF2D20; padding: 15px; color: white; border-radius: 15px;">
                                     Личный кабинет
                                 </a>
                             @else
